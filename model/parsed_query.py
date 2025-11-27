77d12dcea019430ea1b6feb21cf35dd379b57059
nayakazna <nayaka.zna@gmail.com>
2025-11-27 12:25:27 +0700

    fix: update query_tree type hint to Optional for better clarity

--- model/parsed_query.py
+++ model/parsed_query.py
@@ -1,6 +1,7 @@
+from typing import Optional
 from model.query_tree import QueryTree
 
 class ParsedQuery:
-    def __init__(self, query: str, query_tree: QueryTree = None):
+    def __init__(self, query: str, query_tree: Optional[QueryTree] = None):
         self.query = query
         self.query_tree = query_tree
